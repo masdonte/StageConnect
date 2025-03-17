ece47ad2e739f71201036ea5e70120536974c6fc
Valentin AKA BINAK <v.binaku05@gmail.com>
2025-03-17 11:02:49 +0100

    [fix] button to register

--- script.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.getElementsByClassName("login");
-
-if (buttons.length > 0) {
-  const button = buttons[0]; // Sélectionne le premier élément avec la classe "login"
-  button.addEventListener("click", (event) => {
-    button.textContent = `Click count: ${event.detail}`;
-  });
-}
