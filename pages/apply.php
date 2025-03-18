2bfa5ea38067c2a637bd83413e5ababdcfb91edf
Hassanzadeh <kamdine.hassanzadeh@outlook.com>
2025-03-18 10:41:13 +0100

    [ADD] Index
    Refonte index.php

--- pages/apply.php
+++ pages/apply.php
@@ -1,36 +1,11 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestion des Stages</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <title>Document</title>
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="/STAGE-SIO1/pages/index.php">Accueil</a></li>
-                <li><a href="/STAGE-SIO1/pages/offer.php">Offres de stage</a></li>
-                <li><a href="/STAGE-SIO1/pages/apply.php">Postuler</a></li>
-                <li><a href="/STAGE-SIO1/pages/review.php">Témoignages</a></li>
-            </ul>
-        </nav>
-        <div class="banner">
-            <div class="titleMain">
-                <h1>Gestion de stages</h1>
-                <p>Votre avenir commence ici</p>
-            </div>
-            <div class="buttons">
-                <button><a href="/STAGE-SIO1/pages/login.php">Se connecter</button>
-                <button>S'inscrire</button>
-            </div>
-        </div>
-    </header>
-    <footer>
-        <p>&copy; 2025 Gestion des Stages. Tous droits réservés. ASCI Chopin</p>
-    </footer>
     
-    <script src="script.js"></script>
 </body>
 </html>
