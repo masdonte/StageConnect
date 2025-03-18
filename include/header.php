0063e948b3227be732251f9695d1c6fefdc4b8ea
unknown <reda.oukhira@SIO.local>
2025-03-18 16:11:11 +0100

    [FIX] header.php

--- include/header.php
+++ include/header.php
@@ -1,17 +1,18 @@
 <nav>
-      <a class="logo">StageConnect<span>.</span></a>
-      <ul>
-         <li>StageConnect.</li>
-         <li><a href="/STAGE-SIO1/pages/index.php">Accueil</a></li>
-         <li><a href="/STAGE-SIO1/pages/offer.php">Offres de stage</a></li>
-         <li><a href="/STAGE-SIO1/pages/apply.php">Postuler</a></li>
-         <li><a href="/STAGE-SIO1/pages/review.php">Avis</a></li>
-         <?php
-         if ($_SESSION["connected"]&&isset( $_SESSION["connected"])){
-            ?><li>Bienvenue <?php
-            echo $_SESSION["nom"];
-            ?></li><?php
-         }
+   <a class="logo">StageConnect<span>.</span></a>
+   <ul>
+      <li>StageConnect.</li>
+      <li><a href="/STAGE-SIO1/pages/index.php">Accueil</a></li>
+      <li><a href="/STAGE-SIO1/pages/offer.php">Offres de stage</a></li>
+      <li><a href="/STAGE-SIO1/pages/apply.php">Postuler</a></li>
+      <li><a href="/STAGE-SIO1/pages/review.php">Avis</a></li>
+      <?php
+      if (isset($_SESSION["connected"])&&$_SESSION["connected"]) {
          ?>
-      </ul>
+         <li>Bienvenue <?php
+         echo $_SESSION["nom"];
+         ?></li><?php
+      }
+      ?>
+   </ul>
 </nav>
