c6bb55297f681fbf13dfcc39601d9d2909bac8ad
Valentin AKA BINAK <v.binaku05@gmail.com>
2025-03-18 10:10:06 +0100

    [fix] font style

--- pages/offer.php
+++ pages/offer.php
@@ -24,7 +24,7 @@ $stmt = $pdo->prepare("SELECT * FROM offre");
     <header>
         <nav>
             <ul>
-                <li><a href="../index.php">Accueil</a></li>
+                <li><a href="/STAGE-SIO1/pages/index.php">Accueil</a></li>
                 <li><a href="/STAGE-SIO1/pages/gestion.php">Gestion de vos offres de stage</a></li>
                 <li><a href="/STAGE-SIO1/pages/apply.php">Postuler</a></li>
                 <li><a href="/STAGE-SIO1/pages/review.php">Témoignages</a></li>
