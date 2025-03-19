d64d6e588dd938b15141cda8daa1b378480e8dd1
unknown <celina.drici@SIO.local>
2025-03-19 14:59:14 +0100

    [ADD]création du FAQ+faq.css

--- pages/faq.php
+++ pages/faq.php
@@ -16,32 +16,39 @@ include "../include/header.php";
 
 <body>
     <div class="questions">
+        <br>
 
         <h1 class="titre">Toutes vos réponses à vos questions.</h1>
+        <br>
+        <br>
+        <br>
 
-        <h2>Comment se connecter ? </h2>
-        <p>Pour se connecter, vous devez vous créez un compte ou sinon vous connectez directement, suite à ça vous pourrez acceder à notre plateforme StageConnect</p>
-
-        <h2>A quoi sert notre site internet ? </h2>
-        <p>Cela vous permet d'acceder à de nombreuse offres des différentes entreprise en fonction de votre spécialité, vous pourrez donc postuler directement pour un stage.</p>
-
+        <h2>Comment se connecter ?</h2>
+        <p>Pour se connecter, vous devez créer un compte ou vous connecter directement. Suite à cela, vous pourrez
+            accéder à notre plateforme StageConnect.</p>
+        <br><br>
+        <h2>À quoi sert notre site internet ?</h2>
+        <p>Il vous permet d'accéder à de nombreuses offres d'entreprises en fonction de votre spécialité, vous pourrez
+            ainsi postuler directement pour un stage.</p>
+        <br><br>
         <h2>Qui sont les créateurs de cette plateforme ?</h2>
         <p>Les créateurs de cette plateforme sont Kamdine, Valentin, Réda et Célina.</p>
-
-        <h2>Pouvez-vous voir les avis ou communiquer directement entre vous ?</h2>
-        <p>Vous pouvez écrire différentes avis tout simplement en étant connecter qui vous permez de partager votre expérience ou votre avis sur notre plateforme.</p>
-
+        <br><br>
+        <h2>Pouvez-vous écrire votre avis ou communiquer directement entre vous ?</h2>
+        <p>Vous pouvez écrire différents avis tout simplement en étant connecté, ce qui vous permet de partager votre
+            expérience ou votre avis sur notre plateforme.</p>
+        <br><br>
         <h2>Quelles sont les erreurs à éviter lors de la recherche d'emploi ?</h2>
-        <p>Évitez d'envoyer des candidatures génériques,de ne pas
-            suivre les instructions de candidature, et de ne pas préparer d'entretien.</p>
+        <p>Évitez d'envoyer des candidatures génériques, de ne pas suivre les instructions de candidature et de ne pas
+            préparer l'entretien.</p>
+        <br><br>
+        <h2>Quel est le but de ce site internet ?</h2>
+        <p>Nous avons créé ce site internet dans le but d'aider toutes les personnes en BTS SIO dans la recherche d'un
+            stage.</p>
 
-        <h2>Quelle est le but de ce site internet ?</h2>
-        <p>Nous avons créer ce site internet dans le but d'aider toute personnes en BTS SIO dans la recherche d'un stage.</p>
     </div>
 
-
+    <?php include('../include/footer.php'); ?>
 </body>
-<footer>
-  <p>&copy; 2025 Gestion des Stages. Tous droits réservés. ASCI Chopin</p>
-</footer>
+
 </html>
