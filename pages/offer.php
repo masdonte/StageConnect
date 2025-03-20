ee482af527cbec2d7d631263c47f136678eba47c
celinaninadr <dricicelina@icloud.com>
2025-03-20 15:17:12 +0100

    update offre

--- pages/offer.php
+++ pages/offer.php
@@ -9,9 +9,9 @@ include('./config.php'); ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion des Stages</title>
-        <?php 
-        include "../include/header.php"; 
-            ?> 
+    <?php
+    include "../include/header.php";
+    ?>
     <!--=============== CSS ===============-->
     <link rel="stylesheet" href="../css/offer.css">
 
@@ -29,29 +29,26 @@ include('./config.php'); ?>
         foreach ($offres as $offre) {
             ?>
             <div class="card">
-                <div class="card-img-holder">
-                    <img src="/STAGE-SIO1/asset/worker.png" alt="Blog image">
-                </div>
                 <h3 class="blog-title">
                     <?php echo $offre["Nom_de_l_entreprise"]; ?>
                 </h3>
                 <span class="blog-time"></span>
                 <p class="description">
-                    <p>Lieu de Stage :<?php  echo $offre["Lieu_de_Stage"]; ?></p>
-            
-                    <p>Nom de l'entreprise :<?php echo $offre["Nom_de_l_entreprise"]; ?></p>
-                    
-                    <p>Adresse : <?php echo $offre["Adresse"]; ?></p>
-                    
-                   <p>Mail :<?php echo $offre["Mail"]; ?></p> 
-                    
-                    <p>Numéro de Téléphone :<?php echo $offre["Numero_de_telephone"]; ?></p>
-                   
+                <p>Lieu de Stage :<?php echo $offre["Lieu_de_Stage"]; ?></p>
+
+                <p>Nom de l'entreprise :<?php echo $offre["Nom_de_l_entreprise"]; ?></p>
+
+                <p>Adresse : <?php echo $offre["Adresse"]; ?></p>
+
+                <p>Mail :<?php echo $offre["Mail"]; ?></p>
+
+                <p>Numéro de Téléphone :<?php echo $offre["Numero_de_telephone"]; ?></p>
+
                 </p>
                 <div class="options">
                     <span>
                         <?php echo $offre["Adresse"]; ?>
-                       
+
 
                     </span>
                     <button class="btn">Postuler</button>
