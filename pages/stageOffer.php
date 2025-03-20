34ddb230d4a5dec65ad38f28bc01486252c8e5e9
Valentin AKA BINAK <v.binaku05@gmail.com>
2025-03-20 15:27:37 +0100

    [add]stageOffer pour modifier les données

---
+++ pages/stageOffer.php
@@ -0,0 +1,13 @@
+<?php
+include("../include/permission.php");
+include('./config.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion des Stages</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/stageOffer.css">
+</head>
