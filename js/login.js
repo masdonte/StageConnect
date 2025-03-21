edd16e08479def46499e1ba050a3d79e993d84d8
Hassanzadeh <kamdine.hassanzadeh@outlook.com>
2025-03-21 09:47:12 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,14 +1,14 @@
-/*=============== AFFICHER OU CACHER LE MOT DE PASSE ===============*/
+/*=============== SHOW HIDE PASSWORD LOGIN ===============*/
 const passwordAccess = (loginPass, loginEye) =>{
     const input = document.getElementById(loginPass),
           iconEye = document.getElementById(loginEye)
  
     iconEye.addEventListener('click', () =>{
-       // Change password to text
+       
        input.type === 'password' ? input.type = 'text'
                                        : input.type = 'password'
  
-       // Icon change
+       
        iconEye.classList.toggle('ri-eye-fill')
        iconEye.classList.toggle('ri-eye-off-fill')
     })
@@ -21,11 +21,11 @@ const passwordAccess = (loginPass, loginEye) =>{
           iconEye = document.getElementById(loginEye)
  
     iconEye.addEventListener('click', () =>{
-       // Change password to text
+       
        input.type === 'password' ? input.type = 'text'
                                        : input.type = 'password'
  
-       // Icon change
+      
        iconEye.classList.toggle('ri-eye-fill')
        iconEye.classList.toggle('ri-eye-off-fill')
     })
